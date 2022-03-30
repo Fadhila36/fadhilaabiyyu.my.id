b28c3f5af105a1923889bf08294f9985ccc62dd9
Muhammad Fadhila Abiyyu Faris <if18.muhammadabiyyu@mhs.ubpkarawang.ac.id>
2022-03-31 02:31:01 +0700

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Muhammad Fadhila Abiyyu Faris</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
