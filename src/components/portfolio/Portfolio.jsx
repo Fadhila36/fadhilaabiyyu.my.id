1949c8b86d1fb8e1cf3181e90a825fd0bb08c94e
Muhammad Fadhila Abiyyu Faris <if18.muhammadabiyyu@mhs.ubpkarawang.ac.id>
2022-04-01 02:24:20 +0700

    testimoni,swiper,fix tampilan

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,127 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from "../../assets/absensi.png";
+import IMG2 from "../../assets/chat.png";
+import IMG3 from "../../assets/Futsalin.png";
+import IMG4 from "../../assets/kearsipan.png";
+import IMG5 from "../../assets/Lapak-sayur.png";
+import IMG6 from "../../assets/lppm.png";
+import IMG7 from "../../assets/pgsd.png";
+import IMG8 from "../../assets/ppkn.png";
+import IMG9 from "../../assets/service.png";
+import IMG10 from "../../assets/surat.png";
 
-function Portfolio() {
-  return <section id="portfolio">Portfolio</section>;
-}
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Crypto Currency Dashboard & Financial Virtualization",
+    github: "https://github.com/",
+    demo: "https://github.com/",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Charts templates & infographics in figma",
+    github: "https://github.com/",
+    demo: "https://github.com/",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Figma Dashboard UI kit for data design web apps",
+    github: "https://github.com/",
+    demo: "https://github.com/",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Maintaining tasks and tracking progress",
+    github: "https://github.com/",
+    demo: "https://github.com/",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Charts templates & infographics in figma",
+    github: "https://github.com/",
+    demo: "https://github.com/",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Charts templates & infographics in figma",
+    github: "https://github.com/",
+    demo: "https://github.com/",
+  },
+  {
+    id: 7,
+    image: IMG7,
+    title: "Charts templates & infographics in figma",
+    github: "https://github.com/",
+    demo: "https://github.com/",
+  },
+  {
+    id: 8,
+    image: IMG8,
+    title: "Charts templates & infographics in figma",
+    github: "https://github.com/",
+    demo: "https://github.com/",
+  },
+  {
+    id: 9,
+    image: IMG9,
+    title: "Charts templates & infographics in figma",
+    github: "https://github.com/",
+    demo: "https://github.com/",
+  },
+  {
+    id: 10,
+    image: IMG10,
+    title: "Charts templates & infographics in figma",
+    github: "https://github.com/",
+    demo: "https://github.com/",
+  },
+];
+
+const Portfolio = () => {
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {data.map(({ id, image, demo, github, title }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a
+                  href={github}
+                  className="btn"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
 export default Portfolio;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,82 @@
 import React from "react";
 import "./testimonials.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR4 from "../../assets/avatar4.jpg";
 
-function Testimonials() {
-  return <section id="testimonials">Testimonials</section>;
-}
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
+
+// Import Swiper styles
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+// Testimonials Data
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Tina Snow",
+    review: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error non
+    harum vel, iusto, laboriosam dolor autem aliquid commodi tempore aut
+    velit, fugiat consequatur fuga sunt earum id sapiente consequuntur
+    neque.`,
+  },
+  {
+    avatar: AVTR4,
+    name: "Shatta Wale",
+    review: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error non
+    harum vel, iusto, laboriosam dolor autem aliquid commodi tempore aut
+    velit, fugiat consequatur fuga sunt earum id sapiente consequuntur
+    neque.`,
+  },
+  {
+    avatar: AVTR3,
+    name: "Kwame Despite",
+    review: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error non
+    harum vel, iusto, laboriosam dolor autem aliquid commodi tempore aut
+    velit, fugiat consequatur fuga sunt earum id sapiente consequuntur
+    neque.`,
+  },
+  {
+    avatar: AVTR2,
+    name: "Nana Ama McBrown",
+    review: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error non
+    harum vel, iusto, laboriosam dolor autem aliquid commodi tempore aut
+    velit, fugiat consequatur fuga sunt earum id sapiente consequuntur
+    neque.`,
+  },
+];
+
+const Testimonials = () => {
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper
+        className="container testimonials__container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
 
 export default Testimonials;
